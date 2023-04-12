b42e765bae75a74fe46f42b9dc68764e02a24b7d
杜宇 <dygood@outlook.com>
2023-04-12 17:11:50 +0800

    添加路由

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,8 @@ import { ConfigProvider, theme } from 'antd';
 import { Home } from '@/pages/home';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Routes, Route, HashRouter } from 'react-router-dom';
+import { About } from '@/pages/about';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -15,7 +17,12 @@ root.render(
         }
       }}
     >
-      <Home />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </HashRouter>
     </ConfigProvider>
   </React.StrictMode>
 );
